b23362e6518677fd0e3d0dba593f1ee3d3554998
Siddardha Dantuluri <d.siddardha@gmail.com>
2018-04-20 00:13:35 -0400

    commit3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'pm-root',
-  templateUrl:'./products/product-list.component.html'
+  templateUrl:'./app.component.html'
 })
 export class AppComponent{
   PageTitle : String = 'Acme Product Management';
