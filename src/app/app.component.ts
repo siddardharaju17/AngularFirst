58445e5083d180cf538f1156ec65f213af54d4b8
Siddardha Dantuluri <d.siddardha@gmail.com>
2018-04-19 23:34:32 -0400

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'pm-root',
-  template:`<h1>{{PageTitle}}</h1>
-            <div>This is my First Component</div>`
+  templateUrl:'./products/product-list.component.html'
 })
 export class AppComponent{
   PageTitle : String = 'Acme Product Management';
